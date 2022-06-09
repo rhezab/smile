73adba7c3bd869aa9ca6981c24e72023455a0b10
todd gureckis <gureckis@mac.com>
2022-06-09 17:39:39 -0400

    dont need this

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import progress from 'vite-plugin-progress'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), progress()],
+  plugins: [vue()],
   envDir: 'env'
 })
