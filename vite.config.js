79fc5a8c02862504bbef2f83f32b8e1931520fe5
todd gureckis <gureckis@mac.com>
2022-06-10 12:27:46 -0400

    need to direct to correct subfolder

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  process.env = {...process.env, ...loadEnv(mode, process.cwd()+"/env/")};
 
   // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
   // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
-
+  console.log(process.env)
   return defineConfig({
       plugins: [vue()],
       envDir: 'env',
