0af1878cbfb656bd852f7a81a332b98e2857fe4f
todd gureckis <gureckis@mac.com>
2022-06-15 14:30:18 -0500

    don’t try docs deploy if you aren’t pushing to the base

--- scripts/update_config.sh
+++ scripts/update_config.sh
@@ -6,7 +6,23 @@ ENC=$(cat env/.env.local | base64)
 gh secret set SECRET_APP_CONFIG --body "$ENC"
 
 # update doc secrets (these go one variable at a time)
-gh secret set -f env/.env.docs.local
+# only do this if repo name is nyuccl/docs
+url=$(git config --get remote.origin.url)
+
+re="^(https|git)(:\/\/|@)([^\/:]+)[\/:]([^\/:]+)\/(.+)(.git)*$"
+
+if [[ $url =~ $re ]]; then    
+    protocol=${BASH_REMATCH[1]}
+    separator=${BASH_REMATCH[2]}
+    hostname=${BASH_REMATCH[3]}
+    user=${BASH_REMATCH[4]}
+    repo=${BASH_REMATCH[5]}
+fi
+
+if [[ "$user/$repo" == "NYUCCL/smile.git" ]]
+then
+    gh secret set -f env/.env.docs.local
+fi
 
 # update deploy secrets (these go one variable at a time)
 gh secret set -f env/.env.deploy.local
