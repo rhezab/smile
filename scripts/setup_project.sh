d795a19e59dcf5ad4e3240b414767d6fb07a0c97
todd gureckis <gureckis@mac.com>
2022-06-21 13:38:58 -0500

    clarify

--- scripts/setup_project.sh
+++ scripts/setup_project.sh
@@ -1,6 +1,6 @@
 git reset $(git commit-tree HEAD^{tree} -m "Initial import")
 rm .github/workflows/docs-deploy.yml
 git add .github/workflows/docs-deploy.yml
-git commit -m "removing deploy script"
+git commit -m "removing docs deploy script"
 git push --force
 npm install
