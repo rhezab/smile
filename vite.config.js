b91f5825fd1606cfb4b99fb724e5c5798f2135c7
todd gureckis <gureckis@mac.com>
2022-06-25 15:52:50 -0500

    adding git environment file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd()+"/env/", ''), ...loadEnv('deploy', process.cwd()+"/env/", '')};
+  process.env = {...process.env, ...loadEnv(mode, process.cwd()+"/env/", ''), ...loadEnv('deploy', process.cwd()+"/env/", ''), ...loadEnv('git', process.cwd()+"/env/", '')};
 
   // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
   // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
