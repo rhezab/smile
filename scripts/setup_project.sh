5e32873c214a9edac2a331d47f09a0758a0f96f2
todd gureckis <gureckis@mac.com>
2022-06-25 23:10:49 -0500

    docs deploy was accidently deleted
    - replacing doc deploy script
    - fixing setup_project.sh so it won’t delete docs deploy script if working on the nyuccl/smile repo

--- scripts/setup_project.sh
+++ scripts/setup_project.sh
@@ -7,17 +7,17 @@ ln -s ../../scripts/post-commit .git/hooks/post-commit
 
 PROJECT_NAME=$(basename $(git remote get-url origin) .git)
 OWNER=$(basename $(dirname $(git remote get-url origin)))
-if [ "$OWNER" == "NYUCCL" ] && [ "$PROJECT_NAME" == 'smile'] ; then
+if [ "$OWNER" == "NYUCCL" ] && [ "$PROJECT_NAME" == "smile" ] ; then
     echo "Forcing an empty commit to trigger an initial deploy"
     git commit --allow-empty -m "forcing a deploy"
-    git push
 else
     echo "Removing deploy script and generating initial deploy"
     rm .github/workflows/docs-deploy.yml
     git add .github/workflows/docs-deploy.yml
     git commit -m "initial setup: removing docs deploy script"
-    git push
 fi
+
+git push
 
 echo "Installing dependencies"
 npm install
