cde4a4e070340525560980f5e4b3f2c64cba0a09
todd gureckis <gureckis@mac.com>
2022-06-30 02:41:15 -0500

    i thought i had fixed this already

--- src/components/pages/AdvertisementPage.vue
+++ src/components/pages/AdvertisementPage.vue
@@ -9,8 +9,10 @@ const { next, prev } = useStepRoute()
 </script>
 
 <template>
-    <h1 class="title is-3">Welcome to</h1>
-    <p class="is-family-monospace">{{ smileconfig.code_name }}</p>
-    <br>
-    <button class="button is-success is-light" @click="$router.push(next)">next</button>
+    <div class="prompt">
+        <h1 class="title is-3">Welcome to</h1>
+        <p class="is-family-monospace">{{ smileconfig.code_name }}</p>
+        <br>
+        <button class="button is-success is-light" @click="$router.push(next)">next</button>
+    </div>
 </template>
