64cb657af66ba608033a9be07ee554f75d782e5a
todd gureckis <gureckis@mac.com>
2022-07-01 13:18:17 -0500

    configuring main html page title from the code name
    https://github.com/lxs24sxl/vite-plugin-html-env

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import vitePluginHtmlEnv from 'vite-plugin-html-env'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
@@ -16,7 +17,7 @@ export default ({ mode }) => {
   // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
   // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
   return defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), vitePluginHtmlEnv()],
     envDir: 'env',
     base: process.env.VITE_DEPLOY_BASE_PATH,
     server: {
