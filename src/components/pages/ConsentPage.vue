46eeac6f5fcdfe563163b64b32021795aa3df1ef
todd gureckis <gureckis@mac.com>
2022-07-03 16:28:14 -0500

    nicer toggle

--- src/components/pages/ConsentPage.vue
+++ src/components/pages/ConsentPage.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import useStepRoute from '@/composables/StepRoute'
 
+import Toggle from '@vueform/toggle' // https://github.com/vueform/toggle
 import InformedConsentText from '@/components/atoms/InformedConsentText.vue';
 const router = useRouter()
 const route = useRoute()
@@ -26,32 +27,55 @@ const agree = ref(false)
 <template>
     <div class="content">
         <div class="columns">
+            <div class="column has-background-info-light informedconsent">
+                <InformedConsentText/>
+            </div>
             <div class="column is-one-third">
-                <p class="is-size-8 leftalign emph">
+                <p class="leftalign emph">
                     Informed consent is an important part of a research study verifying that you are 
                     participating willingly and know your rights. Please take the time to read
                     the consent form (you can scroll the page if it not entirely visible).
                 </p>
                 <hr>
-                <p class="leftalign"><input type="checkbox" v-model='agree'/> I have read 
-                and agree to the terms of the consent form.  I am also over 18 years old.</p>
+
+
+   
+
+                
+
+                <p class="leftalign">
+                    I have read and agree to the terms of the 
+                    consent form.  I am also over 18 years old.
+                </p>
+
+                <p>
+                <Toggle v-model="agree"  
+                on-label="I consent!"
+                off-label="Continue" 
+                class="widetoggle"/>
+                </p>
+
                 <hr>
                 <button class="button is-success is-light" v-if='agree' @click="finish(next)">
                     Let's start &nbsp;<fa-icon icon="fa-solid fa-arrow-right" />
                 </button>
 
             </div>
-            <div class="column has-background-info-light informedconsent">
-                <InformedConsentText/>
-            </div>
+
         </div>
     </div>
 </template>
 
 <style scoped>
 
+.widetoggle{
+  --toggle-width: 6rem;
+}
+
+
 .leftalign {
     text-align: left;
+    font-size: 0.9em;
 }
 
 .emph {
@@ -63,6 +87,10 @@ const agree = ref(false)
 
 .column {
     padding: 40px;
+}
+
+.form {
+    padding: 0px;
 }
 
 .informedconsent {
