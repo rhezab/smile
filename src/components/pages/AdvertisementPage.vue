fc82653c92424b271a7aa5ebdf3c3a947da03f20
todd gureckis <gureckis@mac.com>
2022-07-20 01:45:32 -0500

    remove test

--- src/components/pages/AdvertisementPage.vue
+++ src/components/pages/AdvertisementPage.vue
@@ -30,7 +30,6 @@ function finish(goto) {
         <h1 class="title is-3">Please help us understand the mind!</h1>
         <p>
             Take part in a short experiment where you play a game for money.
-            {{ Math.random() }}
         </p>
         <br>
         <button class="button is-warning" id='finish' @click="finish(next)">I'm ready! &nbsp;<fa-icon icon="fa-solid fa-arrow-right" /></button>
