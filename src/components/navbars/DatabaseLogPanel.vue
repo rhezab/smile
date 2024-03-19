69860fdcafb92079874c47b4224179494a35cccb
todd gureckis <gureckis@mac.com>
2024-03-18 23:00:22 -0400

    updates to the database log layout
    - still not implemented by picking styles

--- src/components/navbars/DatabaseLogPanel.vue
+++ src/components/navbars/DatabaseLogPanel.vue
@@ -6,15 +6,31 @@ const api = SmileAPI()
   <!-- content of panel here -->
   <div class="contentpanel">
     <div class="scrollablelog">
-      <div class="logentry" v-for="i in [...Array(50).keys()]">hi {{ i }}</div>
+      <aside class="menu">
+        <ul class="menu-list">
+          <li v-for="i in [...Array(50).keys()]">
+            <a
+              ><FAIcon icon=" fa-solid fa-angle-right" /> 04-05-2004 @8:34: <b>hifdf {{ i }}</b></a
+            >
+          </li>
+        </ul>
+      </aside>
     </div>
   </div>
   <div class="searchbar"></div>
 </template>
 
 <style scoped>
+.columnheader {
+  background: #f4f4f4;
+  color: #858484;
+  padding: 5px;
+  font-size: 0.8em;
+  margin-bottom: 10px;
+  margin-top: 0px;
+}
 .contentpanel {
-  padding-left: 20px;
+  padding-left: 0px;
   margin-right: 0px;
   margin-bottom: 0px;
   height: 100%;
@@ -28,5 +44,10 @@ const api = SmileAPI()
   overflow-y: scroll;
   display: flex;
   flex-direction: column-reverse;
+}
+.menu-list li {
+  font-size: 0.8em;
+  font-family: monospace;
+  border-bottom: 1px solid #f2f2f2;
 }
 </style>
