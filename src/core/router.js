011d7792861a7c2633de50553f45e63bee690efb
todd gureckis <gureckis@mac.com>
2024-03-29 23:08:30 -0400

    fix incorrect warnings (closes #112)
    the order of the route rules just needs to consider subtimeline actions before considering “forced jumps” from the dev mode

--- src/core/router.js
+++ src/core/router.js
@@ -66,6 +66,18 @@ function addGuards(r) {
       return true
     }
 
+    // if the next route is a subtimeline and you're trying to go to a subtimeline route, allow it
+    // this is necessary because from.meta.next won't immediately get the subroute as next when the subtimeline is randomized
+    if (
+      from.meta !== undefined &&
+      from.meta.next !== undefined &&
+      from.meta.next.type === 'randomized_sub_timeline' &&
+      to.meta.subroute
+    ) {
+      smilestore.setLastRoute(to.name)
+      smilestore.recordRoute(to.name)
+      return true
+    }
     // if you're in jumping mode
     // or you're in presentation mode allow the new route
     if (
@@ -83,18 +95,6 @@ function addGuards(r) {
       return true
     }
 
-    // if the next route is a subtimeline and you're trying to go to a subtimeline route, allow it
-    // this is necessary because from.meta.next won't immediately get the subroute as next when the subtimeline is randomized
-    if (
-      from.meta !== undefined &&
-      from.meta.next !== undefined &&
-      from.meta.next.type === 'randomized_sub_timeline' &&
-      to.meta.subroute
-    ) {
-      smilestore.setLastRoute(to.name)
-      smilestore.recordRoute(to.name)
-      return true
-    }
     // if you're trying to go to the same route you're already on, allow it
     if (smilestore.lastRoute === to.name) {
       return true
